bedf4539d68bf184a5ba92374d5a91d9ec5bf412
YAMASAKI Masahide <masahide.y@gmail.com>
2023-03-21 08:28:11 +0900

    fix

--- sample3/sample3.ino
+++ sample3/sample3.ino
@@ -4,19 +4,16 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
-SoftwareSerial mySerial(D3, D4);
 
+const byte numChars = 32;
 bool displayOn = true;
 int prevTimerCount = 0;
 bool timerStarted = false;
 unsigned long timerStartMillis = 0;
-unsigned long timerStopMillis = 0;
-unsigned long timerDisplayOffMillis = 0;
-unsigned long serialUpdateMillis = 0;
-
-const byte numChars = 32;
 char receivedChars[numChars];
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+SoftwareSerial mySerial(D3, D4);
 
 void setup() {
     WiFi.mode(WIFI_OFF);
@@ -43,6 +40,7 @@ void loop() {
 }
 
 void getMachineInput() {
+    static unsigned long serialUpdateMillis = 0;
     static byte ndx = 0;
     char endMarker = '\n';
     char rc;
@@ -72,6 +70,8 @@ void getMachineInput() {
 
 
 void detectChanges() {
+    static unsigned long timerDisplayOffMillis = 0;
+    static unsigned long timerStopMillis = 0;
 
     if (!timerStarted && receivedChars[25] == '1') {
         timerStartMillis = millis();
@@ -89,7 +89,6 @@ void detectChanges() {
             timerStarted = false;
             timerStopMillis = 0;
             timerDisplayOffMillis = millis();
-            display.invertDisplay(false);
             Serial.println("Stop pump");
             prevTimerCount = (millis() - timerStartMillis) / 1000;
         }
@@ -124,84 +123,75 @@ String getTimer() {
 void updateDisplay() {
     display.clearDisplay();
     if (displayOn) {
-        if (false) {
-            display.setTextSize(7);
-            display.setCursor(25, 8);
-            display.print(getTimer());
-        } else {
-            // draw line
-            display.drawLine(74, 0, 74, 63, SSD1306_WHITE);
-            // draw time seconds
-            display.setTextSize(4);
-            display.setCursor(display.width() / 2 - 1 + 17, 20);
-            if (timerStarted) 
-                display.setTextColor(BLACK, WHITE);
-            else
-                display.setTextColor(WHITE, BLACK);
-            display.print(getTimer());
-            display.setTextColor(WHITE, BLACK);
-            // draw machine state C/S
-            if (receivedChars[0]) {
-                display.setTextSize(2);
-                display.setCursor(1, 1);
-                if (String(receivedChars[0]) == "C") {
-                    display.print("C");
-                } else if (String(receivedChars[0]) == "V") {
-                    display.print("S");
-                } else {
-                    display.print("X");
-                }
-            }
-            if (String(receivedChars).substring(18, 22) == "0000") {
-                // not in boost heating mode
-                // draw fill circle if heating on
-                if (String(receivedChars[23]) == "1") {
-                    display.fillCircle(45, 7, 6, SSD1306_WHITE);
-                }
-                // draw empty circle if heating off
-                if (String(receivedChars[23]) == "0") {
-                    display.drawCircle(45, 7, 6, SSD1306_WHITE);
-                }
+        // draw line
+        display.drawLine(74, 0, 74, 63, SSD1306_WHITE);
+        // draw time seconds
+        display.setTextSize(4);
+        display.setCursor(display.width() / 2 - 1 + 17, 20);
+        display.print(getTimer());
+        display.setTextColor(WHITE, BLACK);
+        // draw machine state C/S
+        if (receivedChars[0]) {
+            display.setTextSize(2);
+            display.setCursor(1, 1);
+            if (String(receivedChars[0]) == "C") {
+                display.print("C");
+            } else if (String(receivedChars[0]) == "V") {
+                display.print("S");
             } else {
-                // in boost heating mode
-                // draw fill rectangle if heating on
-                if (String(receivedChars[23]) == "1") {
-                    display.fillRect(39, 1, 12, 12, SSD1306_WHITE);
-                }
-                // draw empty rectangle if heating off
-                if (String(receivedChars[23]) == "0") {
-                    display.drawRect(39, 1, 12, 12, SSD1306_WHITE);
-                }
+                display.print("X");
             }
-            // draw temperature
-            if (receivedChars[14] && receivedChars[15] && receivedChars[16]) {
-                display.setTextSize(3);
-                display.setCursor(1, 20);
-                if (String(receivedChars[14]) != "0") {
-                    display.print(String(receivedChars[14]));
-                }
-                display.print(String(receivedChars[15]));
-                display.print(String(receivedChars[16]));
-                display.setTextSize(2);
-                display.print((char)247);
-                if (String(receivedChars[14]) == "0") {
-                    display.print("C");
-                }
+        }
+        if (String(receivedChars).substring(18, 22) == "0000") {
+            // not in boost heating mode
+            // draw fill circle if heating on
+            if (String(receivedChars[23]) == "1") {
+                display.fillCircle(45, 7, 6, SSD1306_WHITE);
             }
-            // draw steam temperature
-            if (receivedChars[6] && receivedChars[7] && receivedChars[8]) {
-                display.setTextSize(2);
-                display.setCursor(1, 48);
-                if (String(receivedChars[6]) != "0") {
-                    display.print(String(receivedChars[6]));
-                }
-                display.print(String(receivedChars[7]));
-                display.print(String(receivedChars[8]));
-                display.setTextSize(1);
-                display.print((char)247);
+            // draw empty circle if heating off
+            if (String(receivedChars[23]) == "0") {
+                display.drawCircle(45, 7, 6, SSD1306_WHITE);
+            }
+        } else {
+            // in boost heating mode
+            // draw fill rectangle if heating on
+            if (String(receivedChars[23]) == "1") {
+                display.fillRect(39, 1, 12, 12, SSD1306_WHITE);
+            }
+            // draw empty rectangle if heating off
+            if (String(receivedChars[23]) == "0") {
+                display.drawRect(39, 1, 12, 12, SSD1306_WHITE);
+            }
+        }
+        // draw temperature
+        if (receivedChars[14] && receivedChars[15] && receivedChars[16]) {
+            display.setTextSize(3);
+            display.setCursor(1, 20);
+            if (String(receivedChars[14]) != "0") {
+                display.print(String(receivedChars[14]));
+            }
+            display.print(String(receivedChars[15]));
+            display.print(String(receivedChars[16]));
+            display.setTextSize(2);
+            display.print((char)247);
+            if (String(receivedChars[14]) == "0") {
                 display.print("C");
             }
         }
+        // draw steam temperature
+        if (receivedChars[6] && receivedChars[7] && receivedChars[8]) {
+            display.setTextSize(2);
+            display.setCursor(1, 48);
+            if (String(receivedChars[6]) != "0") {
+                display.print(String(receivedChars[6]));
+            }
+            display.print(String(receivedChars[7]));
+            display.print(String(receivedChars[8]));
+            display.setTextSize(1);
+            display.print((char)247);
+            display.print("C");
+        }
+        display.invertDisplay(timerStarted);
     }
     display.display();
 }
