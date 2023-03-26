772703feae009cf1386be1ca5ad248919f059f95
YAMASAKI Masahide <masahide.y@gmail.com>
2023-03-26 14:00:20 +0900

    fix

--- marax-probe/marax-probe.ino
+++ marax-probe/marax-probe.ino
@@ -8,19 +8,46 @@
 #include "values.h"
 
 
+#define pumpState (status.csv[25] == '1')
 
+//
 const byte numChars = 32;
-bool displayOn = true;
-int prevTimerCount = 0;
-bool timerStarted = false;
-unsigned long timerStartMillis = 0;
-char receivedChars[numChars];
+
+struct Status {
+  char csv[numChars];
+
+  int timeoutCnt;
+  int timer;
+  bool displayOn;
+  bool changed;
+};
+
+unsigned long timerDisplayOffMillis = 0;
+
+bool status_equal(struct Status *a, struct Status *b) {
+  return strcmp(a->csv, b->csv) == 0
+         && a->timer == b->timer
+         && a->displayOn == b->displayOn
+         && a->timeoutCnt == b->timeoutCnt;
+  // return a->machineState == b->machineState && a->heatingState == b->heatingState && a->pumpState == b->pumpState && a->heatingMode == b->heatingMode && a->currentTemp == b->currentTemp && a->targetTemp  == b->targetTemp && a->steamTemp == b->steamTemp && a->timer == b->timer;
+}
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 SoftwareSerial mySerial(D3, D4);
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+Status status;
+
+
+void publish(Status status) {
+  char buf[60];
+  //sprintf(buf, "%c,%d,%d,%d,%d,%d,%d,%d,%d", status.machineState, status.heatingState, status.pumpState, status.heatingMode, status.currentTemp, status.steamTemp, status.targetTemp, status.timer);
+  sprintf(buf, "%s,%d,%d", status.csv, status.timer, status.timeoutCnt);
+  client.publish(topic, buf);
+  Serial.println("published: " + String(buf));
+}
 
 void setup_wifi() {
   delay(10);
@@ -41,7 +68,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length) {
   // 何もしない
 }
 
@@ -63,15 +90,17 @@ void reconnect() {
 }
 
 void setup() {
-
-
   Serial.begin(9600);
   mySerial.begin(9600);
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  memset(receivedChars, 0, numChars);
+  status.displayOn = true;
+  status.timeoutCnt = 0;
+  status.changed = false;
+  memset(status.csv, 0, numChars);
+  timerDisplayOffMillis = millis();
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
@@ -84,26 +113,30 @@ void setup() {
   client.setCallback(callback);
 }
 
+
 void loop() {
-  static char prevBuf[numChars];
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
-  getMachineInput();
-  if (strcmp(prevBuf,receivedChars)){
-    client.publish(topic, receivedChars);
-    Serial.println("Message published");
-    memcpy(prevBuf, receivedChars, numChars);
+  getMachineInput(status);
+  if (status.changed) {
+    publish(status);
+    status.changed = false;
+    updateDisplay(status);
   }
-  detectChanges();
-  updateDisplay();
 }
 
-void getMachineInput() {
+
+void getMachineInput(Status &status) {
+  static bool timerStarted = false;
   static unsigned long serialUpdateMillis = 0;
+  static unsigned long timerStopMillis = 0;
+  static unsigned long timerStartMillis = 0;
+  static int prevTimerCount = 0;
   static byte ndx = 0;
+  static Status prevStatus;
   char endMarker = '\n';
   char rc;
   while (mySerial.available()) {
@@ -111,150 +144,139 @@ void getMachineInput() {
     rc = mySerial.read();
 
     if (rc != endMarker) {
-      receivedChars[ndx] = rc;
+      status.csv[ndx] = rc;
       ndx++;
       if (ndx >= numChars) {
         ndx = numChars - 1;
       }
     } else {
-      receivedChars[ndx] = '\0';
+      status.csv[ndx] = '\0';
       ndx = 0;
-      Serial.println(receivedChars);
+
+      Serial.println("csv: " + String(status.csv));
+      if (!timerStarted && pumpState) {
+        timerStartMillis = millis();
+        timerStarted = true;
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Start pump");
+      }
+      if (timerStarted && !pumpState) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        if (timerStopMillis == 0) {
+          timerStopMillis = millis();
+        }
+        if (millis() - timerStopMillis > 500) {
+          timerStarted = false;
+          timerStopMillis = 0;
+          Serial.println("Stop pump");
+          prevTimerCount = (millis() - timerStartMillis) / 1000;
+        }
+      } else {
+        timerStopMillis = 0;
+      }
+      int timerCount = (timerStarted) ? ((millis() - timerStartMillis) / 1000) : prevTimerCount;
+      status.timer = (timerCount > 99) ? 99 : timerCount;
+      status.timeoutCnt = 0;
+      status.displayOn = true;
     }
   }
-  if (millis() - serialUpdateMillis > 5000) {
-    serialUpdateMillis = millis();
-    memset(receivedChars, 0, numChars);
-    Serial.println("Request serial update");
-    mySerial.write(0x11);
+  if (millis() - serialUpdateMillis > 6000) {
+      serialUpdateMillis = millis();
+      if (status.timeoutCnt < 99) {
+          status.timeoutCnt++;
+      }
+      if (status.timeoutCnt > 10) {
+          prevTimerCount = 0;
+          status.displayOn = false;
+          Serial.println("Sleep");
+      }
+      memcpy(status.csv, prevStatus.csv, numChars);
+      if (status.timeoutCnt > 2) {
+        status.csv[0] = 'E';
+      }
+      Serial.println("Serial.read() timeoutCnt: "+String(status.timeoutCnt));
+      mySerial.write(0x11);
   }
+    // 現在のStatusと前回のStatusを比較し、変更がある場合のみchangedフラグをtrueに設定
+    if (!status_equal(&status, &prevStatus)) {
+      status.changed = true;
+      prevStatus = status;
+      prevStatus.changed = false;
+    } else {
+      status.changed = false;
+    }
 }
 
 
-void detectChanges() {
-  static unsigned long timerDisplayOffMillis = millis();
-  static unsigned long timerStopMillis = 0;
 
-  if (!timerStarted && receivedChars[25] == '1') {
-    timerStartMillis = millis();
-    timerStarted = true;
-    displayOn = true;
-    digitalWrite(LED_BUILTIN, LOW);
-    timerDisplayOffMillis = millis();
-    Serial.println("Start pump");
-  }
-  if (timerStarted && receivedChars[25] == '0') {
-    digitalWrite(LED_BUILTIN, HIGH);
-    if (timerStopMillis == 0) {
-      timerStopMillis = millis();
-    }
-    if (millis() - timerStopMillis > 500) {
-      timerStarted = false;
-      timerStopMillis = 0;
-      timerDisplayOffMillis = millis();
-      Serial.println("Stop pump");
-      prevTimerCount = (millis() - timerStartMillis) / 1000;
-    }
-  } else {
-    timerStopMillis = 0;
-  }
-  if (!timerStarted && displayOn && timerDisplayOffMillis >= 0 && (millis() - timerDisplayOffMillis > 1000 * 60 * 60)) {
-    timerDisplayOffMillis = 0;
-    prevTimerCount = 0;
-    displayOn = false;
-    Serial.println("Sleep");
-  }
-}
-
-String getTimer() {
-  char outMin[3];
-  int timerCount;
-
-  if (timerStarted) {
-    timerCount = (millis() - timerStartMillis) / 1000;
-  } else {
-    timerCount = prevTimerCount;
-  }
-
-  if (timerCount > 99) {
-    return "99";
-  }
-  sprintf(outMin, "%02u", timerCount);
-  return outMin;
-}
-
-void updateDisplay() {
+void updateDisplay(Status &status) {
   display.clearDisplay();
-  if (displayOn) {
+  if (status.displayOn) {
+    char buf[64];
     // draw line
     display.drawLine(74, 0, 74, 63, SSD1306_WHITE);
     // draw time seconds
     display.setTextSize(4);
     display.setCursor(display.width() / 2 - 1 + 17, 20);
-    display.print(getTimer());
+    sprintf(buf, "%02u", status.timer);
+    display.print(buf);
     display.setTextColor(WHITE, BLACK);
     // draw machine state C/S
-    if (receivedChars[0]) {
+    char machineState = status.csv[0];
+    if (machineState) {
       display.setTextSize(2);
       display.setCursor(1, 1);
-      if (String(receivedChars[0]) == "C") {
+      if (machineState == 'C') {
         display.print("C");
-      } else if (String(receivedChars[0]) == "V") {
+      } else if (machineState == 'V') {
         display.print("S");
       } else {
         display.print("X");
       }
     }
-    if (String(receivedChars).substring(18, 22) == "0000") {
+
+    int heatingMode = String(status.csv).substring(18, 22).toInt();
+    bool heatingState = status.csv[23] == '1';
+    if (heatingMode = 0) {
       // not in boost heating mode
       // draw fill circle if heating on
-      if (String(receivedChars[23]) == "1") {
+      if (heatingState) {
         display.fillCircle(45, 7, 6, SSD1306_WHITE);
-      }
-      // draw empty circle if heating off
-      if (String(receivedChars[23]) == "0") {
+      } else {
         display.drawCircle(45, 7, 6, SSD1306_WHITE);
       }
     } else {
       // in boost heating mode
       // draw fill rectangle if heating on
-      if (String(receivedChars[23]) == "1") {
+      if (heatingState) {
         display.fillRect(39, 1, 12, 12, SSD1306_WHITE);
-      }
-      // draw empty rectangle if heating off
-      if (String(receivedChars[23]) == "0") {
+      } else {
         display.drawRect(39, 1, 12, 12, SSD1306_WHITE);
       }
     }
-    // draw temperature
-    if (receivedChars[14] && receivedChars[15] && receivedChars[16]) {
+    int currentTemp = String(status.csv).substring(14, 17).toInt();
+    if (currentTemp) {
       display.setTextSize(3);
       display.setCursor(1, 20);
-      if (String(receivedChars[14]) != "0") {
-        display.print(String(receivedChars[14]));
-      }
-      display.print(String(receivedChars[15]));
-      display.print(String(receivedChars[16]));
+      display.print(String(currentTemp));
       display.setTextSize(2);
       display.print((char)247);
-      if (String(receivedChars[14]) == "0") {
+      if (currentTemp < 100) {
         display.print("C");
       }
     }
-    // draw steam temperature
-    if (receivedChars[6] && receivedChars[7] && receivedChars[8]) {
+    int steamTemp = String(status.csv).substring(6, 9).toInt();
+    if (steamTemp) {
       display.setTextSize(2);
       display.setCursor(1, 48);
-      if (String(receivedChars[6]) != "0") {
-        display.print(String(receivedChars[6]));
-      }
-      display.print(String(receivedChars[7]));
-      display.print(String(receivedChars[8]));
+      display.print(String(steamTemp));
       display.setTextSize(1);
       display.print((char)247);
       display.print("C");
     }
-    display.invertDisplay(timerStarted);
+    display.invertDisplay(pumpState);
+  } else {
+    display.invertDisplay(false);
   }
   display.display();
 }
