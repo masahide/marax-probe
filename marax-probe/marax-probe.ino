cbedff892f5b8a08de7e267b54a2e72988be35f9
YAMASAKI Masahide <masahide.y@gmail.com>
2023-04-03 00:17:19 +0900

    add weather

--- marax-probe/marax-probe.ino
+++ marax-probe/marax-probe.ino
@@ -1,12 +1,14 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #include <PubSubClient.h>
 #include <TimeLib.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ArduinoJson.h>
 #include "values.h"
 
 #define pumpState (status.csv[25] == '1')
@@ -25,6 +27,122 @@ struct Status {
   unsigned long timeoutCnt;
   bool displayOn;
   bool changed;
+};
+
+struct weather {
+  int temp;
+  uint8_t* icon;
+};
+
+/*
+const uint8_t sunny[]   ={ 0x91,0x42,0x18,0x3d,0xbc,0x18,0x42,0x89};
+const uint8_t rain[]    ={ 0x55,0xaa,0x55,0xaa,0x55,0xaa,0x55,0xaa};
+const uint8_t cloud[]   ={ 0x00,0x00,0x7e,0x81,0x89,0x99,0x71,0x0e};
+const uint8_t showers[] ={ 0x15,0x2a,0x7e,0x81,0x81,0x91,0x71,0x0e};
+const uint8_t snow[]    ={ 0xa5,0x42,0xa5,0x18,0x18,0xa5,0x42,0xa5};
+const uint8_t storm[]   ={ 0x09,0x04,0x08,0x7e,0x81,0x91,0x71,0x0e};
+*/
+
+uint8_t icon00[] = {
+        0b00000000,
+        0b01000010,
+        0b00100100,
+        0b00011000,
+        0b00011000,
+        0b00100100,
+        0b01000010,
+        0b00000000,
+};
+uint8_t icon01[] = {
+        0b00010000,
+        0b01010010,
+        0b00111100,
+        0b00111111,
+        0b11111100,
+        0b00111100,
+        0b01001010,
+        0b00001000,
+};
+uint8_t icon02[] = {
+        0b00010000,
+        0b01010100,
+        0b00111000,
+        0b11111110,
+        0b01000100,
+        0b10000010,
+        0b10000001,
+        0b01111110,
+};
+uint8_t icon03[] = {
+        0b00010000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00111000,
+        0b01000100,
+        0b10000010,
+        0b10000001,
+        0b01111110,
+};
+uint8_t icon04[] = {
+        0b01100000,
+        0b10011000,
+        0b10000100,
+        0b01111000,
+        0b01000100,
+        0b10000010,
+        0b10000001,
+        0b01111110,
+};
+uint8_t icon09[] = {
+        0b00111100,
+        0b01111110,
+        0b11111111,
+        0b11111111,
+        0b10001001,
+        0b00001000,
+        0b00101000,
+        0b00111000,
+};
+uint8_t icon10[] = {
+        0b00101010,
+        0b00011100,
+        0b01111111,
+        0b00011100,
+        0b01111010,
+        0b11111100,
+        0b00010000,
+        0b00110000,
+};
+uint8_t icon11[] = {
+        0b00001111,
+        0b00011110,
+        0b00111100,
+        0b01111111,
+        0b11111110,
+        0b00000100,
+        0b00001000,
+        0b00010000,
+};
+uint8_t icon13[] = {
+        0b01010010,
+        0b11010011,
+        0b00111100,
+        0b00100111,
+        0b11100100,
+        0b00111100,
+        0b11001011,
+        0b01001010,
+};
+uint8_t icon50[] = {
+        0b11111111,
+        0b00000000,
+        0b11111111,
+        0b00000000,
+        0b11111111,
+        0b00000000,
+        0b11111111,
+        0b00000000,
 };
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
@@ -103,6 +221,7 @@ void setup() {
   timeClient.begin();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
+  getWeather();
 }
 
 
@@ -231,7 +350,7 @@ void updateDisplay(Status status) {
   if (status.displayOn) {
     char buf[64];
     // draw line
-    display.drawLine(74, 8, 74, 63, SSD1306_WHITE);
+    display.drawLine(74, 8, 74, 63-8, SSD1306_WHITE);
     // draw time seconds
     display.setTextSize(4);
     display.setCursor(display.width() / 2 - 1 + 17, 20);
@@ -265,9 +384,16 @@ void updateDisplay(Status status) {
         display.drawRect(39-8, 1, 12, 12, SSD1306_WHITE);
       }
     }
+    // datetime
     display.setCursor(display.width() / 2-3 , 0);
     display.setTextSize(1);
     display.print(formattedTime);
+    // weather
+    weather w = getWeather();
+    display.setCursor(display.width()-8*5, display.height() - 8);
+    display.print(String(w.temp) + (char)247 +"C");
+    display.drawBitmap(display.width() - 8, display.height() - 8, w.icon, 8, 8, WHITE);
+
     int currentTemp = atoi(status.csv + 14);
     if (currentTemp) {
       display.setTextSize(3);
@@ -303,4 +429,68 @@ void initDisplay() {
 void outputDisplay(String str) {
   display.print(str);
   display.display();
+}
+
+
+weather getWeather() {
+  static weather prevWeather = { 0, icon00  };
+  static unsigned long updateMillis = 0;
+  weather res = { 0, icon01 };
+  if (updateMillis != 0 && millis() - updateMillis < 1000*60*60) {
+    return prevWeather;
+  }
+  updateMillis = millis();
+  HTTPClient http;
+  http.begin(espClient, weather_url);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println("HTTP Response code: " + String(httpCode));
+    Serial.println("Weather data:");
+    Serial.println(payload);
+
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
+    const char* icon = doc["weather"][0]["icon"];
+    float temperature = doc["main"]["temp"];
+    res.temp = (int)temperature;
+    Serial.println("icon: " + String(icon));
+    Serial.println("temp: " + String(res.temp) + " °C");
+    if (strstr(icon,"01")) {
+      res.icon = icon01;
+    }
+    else if (strstr(icon,"02")){
+      res.icon = icon02;
+    }
+    else if (strstr(icon,"03")){
+      res.icon = icon03;
+    }
+    else if (strstr(icon,"04")){
+      res.icon = icon04;
+    }
+    else if (strstr(icon,"09")){
+      res.icon = icon09;
+    }
+    else if (strstr(icon,"10")){
+      res.icon = icon10;
+    }
+    else if (strstr(icon,"11")){
+      res.icon = icon11;
+    }
+    else if (strstr(icon,"13")){
+      res.icon = icon13;
+    }
+    else if (strstr(icon,"50")){
+      res.icon = icon50;
+    }
+    else {
+      res.icon = icon00;
+    }
+  }
+  else {
+    Serial.println("Error on HTTP request");
+  }
+  http.end();
+  prevWeather = res;
+  return res;
 }
