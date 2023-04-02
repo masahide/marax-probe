af5a56c6160dce26938b0e4ac7ff0f6800e4fd4b
YAMASAKI Masahide <masahide.y@gmail.com>
2023-04-02 16:32:07 +0900

    add datetime

--- marax-probe/marax-probe.ino
+++ marax-probe/marax-probe.ino
@@ -3,19 +3,21 @@
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
-#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <TimeLib.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include "values.h"
-
 
 #define pumpState (status.csv[25] == '1')
 #define backoffStart 1000
 #define csvInitData "Z0.00,000,000,000,0000,0,0"
 #define endMarker '\n'
+#define numChars 64
 
-//
-const byte numChars = 64;
-
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 9 * 3600; // JST (UTC+9)の場合
+const int daylightOffset_sec = 0; // 夏時間を使用しない場合
 
 struct Status {
   char csv[numChars];
@@ -25,22 +27,12 @@ struct Status {
   bool changed;
 };
 
-
-bool status_equal(struct Status *a, struct Status *b) {
-  return strcmp(a->csv, b->csv) == 0
-         && a->timer == b->timer
-         && a->displayOn == b->displayOn
-         && a->timeoutCnt == b->timeoutCnt;
-}
-
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 SoftwareSerial mySerial(D3, D4);
-
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-Status status;
-Status prevStatus;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntpServer, gmtOffset_sec, daylightOffset_sec);
 
 
 void publish(Status status) {
@@ -52,21 +44,25 @@ void publish(Status status) {
 
 void setup_wifi() {
   delay(10);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  String str;
+  str = "\nconnect:" + String(ssid) + "\n";
+  Serial.print(str);
+
+  initDisplay();
+  outputDisplay(str);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    outputDisplay(".");
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  str = "\nConnected!\nIP:" + String(WiFi.localIP().toString()) + "\n";
+  Serial.print(str);
+  initDisplay();
+  outputDisplay(str);
+  delay(3000);
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
@@ -97,12 +93,6 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  status.displayOn = true;
-  status.timeoutCnt = 0;
-  status.changed = false;
-  strcpy(status.csv, csvInitData);
-  prevStatus = status;
-
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextColor(WHITE);
@@ -110,6 +100,7 @@ void setup() {
   mySerial.write(0x11);
 
   setup_wifi();
+  timeClient.begin();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 }
@@ -121,10 +112,9 @@ void loop() {
   }
   client.loop();
 
-  getMachineInput(status);
+  Status status = getMachineInput();
   if (status.changed) {
     publish(status);
-    status.changed = false;
     updateDisplay(status);
   }
 }
@@ -146,18 +136,26 @@ bool validate(char *csv) {
          && isNum(csv + 6, 3) && isNum(csv + 10, 3) && isNum(csv + 14, 3) && isNum(csv + 18, 4);
 }
 
+bool statusEq(struct Status *a, struct Status *b) {
+  return strcmp(a->csv, b->csv) == 0
+         && a->timer == b->timer
+         && a->displayOn == b->displayOn
+         && a->timeoutCnt == b->timeoutCnt;
+}
 
-void getMachineInput(Status &status) {
+Status getMachineInput() {
   static bool prevPumpState = false;
   static unsigned long serialUpdateMillis = 0;
   static unsigned long timerStartMillis = 0;
   static unsigned long backoff = backoffStart;
   static int prevTimerCount = 0;
+  static Status status = { csvInitData, 0, 0, true, false };
+  static Status prevStatus = { csvInitData, 0, 0, true, false };
   int rlen = 0;
   if (mySerial.available() > 0) {
     rlen = mySerial.readBytesUntil(endMarker, status.csv, numChars);
     if (rlen > 0) {
-      status.csv[rlen-1] = 0;
+      status.csv[rlen - 1] = 0;
     }
   }
   if (rlen > 0 && validate(status.csv)) {
@@ -181,8 +179,8 @@ void getMachineInput(Status &status) {
     status.displayOn = true;
   } else {
     // ng
-    if (rlen){
-      Serial.println( "ng csv: " + String(status.csv) + "; len csv: " + String(strlen(status.csv)));
+    if (rlen) {
+      Serial.println("ng csv: " + String(status.csv) + "; len csv: " + String(strlen(status.csv)));
     }
     memcpy(status.csv, prevStatus.csv, numChars);
   }
@@ -191,7 +189,7 @@ void getMachineInput(Status &status) {
 
   if (millis() - serialUpdateMillis > backoff) {
     serialUpdateMillis = millis();
-    if (backoff < backoffStart*20) {
+    if (backoff < backoffStart * 20) {
       backoff = backoff << 1;
     }
     if (status.timeoutCnt < 99) {
@@ -208,24 +206,32 @@ void getMachineInput(Status &status) {
     Serial.println("Serial.read() timeoutCnt: " + String(status.timeoutCnt));
     mySerial.write(0x11);
   }
-  // 現在のStatusと前回のStatusを比較し、変更がある場合のみchangedフラグをtrueに設定
-  if (!status_equal(&status, &prevStatus)) {
+  if (!statusEq(&status, &prevStatus)) {
     status.changed = true;
     prevStatus = status;
     prevStatus.changed = false;
   } else {
     status.changed = false;
   }
+  return status;
+}
+
+String getFormattedDateTime(unsigned long unixTime) {
+  char buffer[16];
+  time_t time = (time_t)unixTime;
+  strftime(buffer, sizeof(buffer), "%m/%d %H:%M", localtime(&time));
+  return String(buffer);
 }
 
 
-
-void updateDisplay(Status &status) {
+void updateDisplay(Status status) {
+  timeClient.update();
+  String formattedTime = getFormattedDateTime(timeClient.getEpochTime());
   display.clearDisplay();
   if (status.displayOn) {
     char buf[64];
     // draw line
-    display.drawLine(74, 0, 74, 63, SSD1306_WHITE);
+    display.drawLine(74, 8, 74, 63, SSD1306_WHITE);
     // draw time seconds
     display.setTextSize(4);
     display.setCursor(display.width() / 2 - 1 + 17, 20);
@@ -233,39 +239,36 @@ void updateDisplay(Status &status) {
     display.print(buf);
     display.setTextColor(WHITE, BLACK);
     // draw machine state C/S
-    char machineState = status.csv[0];
-    if (machineState) {
+    char state = status.csv[0];
+    if (state) {
       display.setTextSize(2);
       display.setCursor(1, 1);
-      if (machineState == 'C') {
-        display.print("C");
-      } else if (machineState == 'V') {
-        display.print("S");
-      } else {
-        display.print("X");
-      }
+      display.print((state == 'C') ? "C" : (state == 'V') ? "S" : "X");
     }
 
-    int heatingMode = String(status.csv).substring(18, 22).toInt();
+    int heatingMode = atoi(status.csv + 18);
     bool heatingState = status.csv[23] == '1';
-    if (heatingMode = 0) {
+    if (heatingMode == 0) {
       // not in boost heating mode
       // draw fill circle if heating on
       if (heatingState) {
-        display.fillCircle(45, 7, 6, SSD1306_WHITE);
+        display.fillCircle(45-8, 7, 6, SSD1306_WHITE);
       } else {
-        display.drawCircle(45, 7, 6, SSD1306_WHITE);
+        display.drawCircle(45-8, 7, 6, SSD1306_WHITE);
       }
     } else {
       // in boost heating mode
       // draw fill rectangle if heating on
       if (heatingState) {
-        display.fillRect(39, 1, 12, 12, SSD1306_WHITE);
+        display.fillRect(39-8, 1, 12, 12, SSD1306_WHITE);
       } else {
-        display.drawRect(39, 1, 12, 12, SSD1306_WHITE);
+        display.drawRect(39-8, 1, 12, 12, SSD1306_WHITE);
       }
     }
-    int currentTemp = String(status.csv).substring(14, 17).toInt();
+    display.setCursor(display.width() / 2-3 , 0);
+    display.setTextSize(1);
+    display.print(formattedTime);
+    int currentTemp = atoi(status.csv + 14);
     if (currentTemp) {
       display.setTextSize(3);
       display.setCursor(1, 20);
@@ -276,7 +279,7 @@ void updateDisplay(Status &status) {
         display.print("C");
       }
     }
-    int steamTemp = String(status.csv).substring(6, 9).toInt();
+    int steamTemp = atoi(status.csv + 6);
     if (steamTemp) {
       display.setTextSize(2);
       display.setCursor(1, 48);
@@ -289,5 +292,15 @@ void updateDisplay(Status &status) {
   } else {
     display.invertDisplay(false);
   }
+  display.display();
+}
+
+void initDisplay() {
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.setTextSize(1);
+}
+void outputDisplay(String str) {
+  display.print(str);
   display.display();
 }
